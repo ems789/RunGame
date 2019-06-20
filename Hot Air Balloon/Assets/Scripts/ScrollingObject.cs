ddbc83c48360e6617fdf5b284488af8a526c0305
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-20 23:56:27 +0900

    Update ScrollingObject.cs

--- Hot Air Balloon/Assets/Scripts/ScrollingObject.cs
+++ Hot Air Balloon/Assets/Scripts/ScrollingObject.cs
@@ -6,7 +6,6 @@ public class ScrollingObject : MonoBehaviour
 {
     public float speed = 10f;
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
