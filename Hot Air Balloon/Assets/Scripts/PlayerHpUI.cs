5f39ad68df7469148c9e2f19186379060b13cd56
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-20 22:27:53 +0900

    PlayerHpUI를 추가
    플레이어 체력에 따라 HP UI를 처리함

---
+++ Hot Air Balloon/Assets/Scripts/PlayerHpUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHpUI : MonoBehaviour
+{
+    public Slider hpBar;
+    private PlayerController player;
+
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        hpBar.value = (float)player.currentHP / (float)player.maxHP;
+    }
+}
