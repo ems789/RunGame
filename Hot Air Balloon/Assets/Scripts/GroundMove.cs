30640ce48916e59a8d45ab4ea87878f521c15818
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-20 16:28:22 +0900

    GroundMove 스크립트 추가
    자식 Ground들에 추가하는 스크립트
    Ground의 Scrolling으로 인해 자식 Ground의 위치가 왼쪽으로 width만큼 이동 했을 때 자식 Ground를 다시 원래 위치로 되돌린다

---
+++ Hot Air Balloon/Assets/Scripts/GroundMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMove : MonoBehaviour
+{
+    private float width;
+
+    void Start()
+    {
+        BoxCollider backgroundColider = GetComponent<BoxCollider>();
+        width = backgroundColider.size.x * transform.localScale.x;
+    }
+
+    void Update()
+    {
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+
+    void Reposition()
+    {
+        // 현재 위치에서 오른쪽으로 가로 길이 *2 만큼 이동
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
