503d1c04bdbe93e1b51fc0a7fba28c011a5094af
ems789 <32154246+ems789@users.noreply.github.com>
2019-06-20 14:38:48 +0900

    PlayerController 스크립트 추가
    마우스or터치가 눌린 상태면 플레이어가 공중 부양

---
+++ Hot Air Balloon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public int hp = 100;
+    public int levitationSpeed = 200;
+    
+    // 레벨
+    // 현재 경험치, 필요 경험치
+    private bool isDead = false;
+
+    void Update()
+    {      
+        if(Input.GetMouseButton(0))
+        {
+            // 일시 정지 상태에서 다시 게임 진행
+            if (Time.timeScale == 0 && isDead == false)
+                Time.timeScale = 1;
+
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(0, speed, 0);
+        }
+
+        // 체력이 시간마다 감소            
+        // 체력이 0이 되면 사망 처리
+        // 게임 매니저의 게임 오버 호출, 재시작 버튼 누르면 재시작
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 땅에 닿으면 게임을 멈춤
+        if(collision.transform.tag == "Ground")
+        {            
+            Time.timeScale = 0;
+        }
+    }
+}
