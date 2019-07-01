c697b943e5dd982dc5edb2b1fe4462a87d67b73d
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-02 08:36:39 +0900

    Air.cs에 트리거 충돌 처리 추가

---
+++ Hot Air Balloon/Assets/Scripts/Air.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Air : MonoBehaviour
+{
+    public int recovery = 4;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player.instance.HPUp(recovery);
+            Destroy(gameObject);
+        }
+    }
+}
