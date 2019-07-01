ab48870bbb40fab5f37070c75b93cc59762a5a11
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-02 07:52:52 +0900

    오브젝트 이동을 관리하는 ObjectMove.cs 추가
    + 일정 시간이 지나면 오브젝트 자동 삭제

---
+++ Hot Air Balloon/Assets/Scripts/ObjectMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private void Start()
+    {
+        Destroy(gameObject, 8f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
