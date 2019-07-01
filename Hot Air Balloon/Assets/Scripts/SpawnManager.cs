e0e5b81b20a7ed22f9c4c0fe96b917277c3b1654
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-01 21:33:07 +0900

    오브젝트  생성을 관리하는 SpawnManager.cs 추가
    3초 간격으로 AIr(에너지)를 생성

---
+++ Hot Air Balloon/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject air;
+    private float airTime = 3.0f;
+
+    private float timeChk;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeChk += Time.deltaTime;
+        while (timeChk >= airTime)
+        {
+            float randomY = Random.Range(Constant.minHeight, Constant.maxHeight);
+            Instantiate(air, new Vector3(transform.position.x, randomY, 0f), Quaternion.identity);
+            timeChk = 0;
+        }
+
+    }
+}
