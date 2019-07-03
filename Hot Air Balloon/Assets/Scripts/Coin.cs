c38f329ed2206ab2cdac13183cd56cf78b4a5c81
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-03 10:36:56 +0900

    Coin.cs 추가
    코인별 점수 구분과 충돌처리를 위함

---
+++ Hot Air Balloon/Assets/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int coinScore; // 코인별 점수 구분
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.instance.GetCoin(coinScore);
+            Destroy(gameObject);
+        }
+    }
+}
