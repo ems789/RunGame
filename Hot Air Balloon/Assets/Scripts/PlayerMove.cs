d8cab19ef1c98cf80f9892d8bad91eb1d3a62e17
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-04 08:38:44 +0900

    플레이어 이동을 관리하는 스크립트의 이름 변경

--- Hot Air Balloon/Assets/Scripts/PlayerMove.cs
+++ Hot Air Balloon/Assets/Scripts/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     Animator anim;
 
