1234b64ed710d018ef34a310fc30669b3f95efd8
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-03 09:00:25 +0900

    적 정보를 가지고 있는 Enemy.cs 추가

---
+++ Hot Air Balloon/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int damage = 10;
+
+    // enum으로 타입 구분, 타입별 효과 추가
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player.instance.GetDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
