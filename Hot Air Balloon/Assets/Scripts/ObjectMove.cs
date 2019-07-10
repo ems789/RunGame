fe35186bd35bb139561b17d0f218baa4b321b31c
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-10 22:32:33 +0900

    오브젝트가 활성화될 때 마다 코루틴이 호출되도록 변경
    Start() -> OnEnable()로 함수 사용 변경

--- Hot Air Balloon/Assets/Scripts/ObjectMove.cs
+++ Hot Air Balloon/Assets/Scripts/ObjectMove.cs
@@ -7,7 +7,7 @@ public class ObjectMove : MonoBehaviour
     public float speed = 5f;
     public float afterTimeDisable = 6f; 
 
-    private void Start()
+    private void OnEnable()
     {
         StartCoroutine(Disabled(afterTimeDisable));
     }
@@ -19,7 +19,7 @@ public class ObjectMove : MonoBehaviour
 
     // 화면을 벗어나는 오브젝트들의 SetActive 상태를 변경하기 위함
     IEnumerator Disabled(float waitTime)
-    {
+    {        
         yield return new WaitForSeconds(waitTime);
         gameObject.SetActive(false);
     }
