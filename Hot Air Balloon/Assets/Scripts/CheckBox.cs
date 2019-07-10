09e1e170742cb94b3c5c416296b3a087fad63b6c
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-10 20:04:24 +0900

    타이틀 화면 구현
    체크리스트, 게임시작시 씬이동 구현

---
+++ Hot Air Balloon/Assets/Scripts/CheckBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckBox : MonoBehaviour
+{
+    private bool isCheck = false;
+    public Image checkImage;
+
+
+    public void OnClick()
+    {
+        Debug.Log("들어옴" + !isCheck);
+        isCheck = !isCheck;
+        if (isCheck)
+            checkImage.enabled = true;
+        else
+            checkImage.enabled = false;
+    }
+}
---
+++ Hot Air Balloon/Assets/Scripts/GameStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public Image checkImage; // 체크리스트
+
+    public void OnClick()
+    { 
+        if(checkImage.enabled) // 체크리스트가 활성화되어 있으면 게임 시작 가능
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
