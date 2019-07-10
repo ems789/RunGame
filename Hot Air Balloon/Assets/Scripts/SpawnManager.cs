d9a07e44a6bdd8e68ef0c946b1cbf91490b8fb19
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-10 22:31:17 +0900

    오브젝트 풀을 관리하는 SpawnManger.cs 추가

--- Hot Air Balloon/Assets/Scripts/SpawnManager.cs
+++ Hot Air Balloon/Assets/Scripts/SpawnManager.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obj;
-    public float[] objTimeInterval;
+    public float[] objTimeInterval; // 생성 간격
+
+    private int maxObj = 20; // 미리 생성해둘 오브젝트 수
+    private ObjectPool[] objPool;
+
+    private void Awake()
+    {
+        objPool = new ObjectPool[obj.Length];
+        for (int i = 0; i < obj.Length; i++)
+            objPool[i] = new ObjectPool();
+
+        for (int i = 0; i < objPool.Length; i++)
+            objPool[i].InitPool(obj[i], maxObj);
+    }
 
     private void Start()
     {
         for(int i=0; i<obj.Length; i++)
         {
-            StartCoroutine(SpawnObject(obj[i], objTimeInterval[i]));
+            StartCoroutine(SpawnObject(objPool[i], objTimeInterval[i]));
         }
     }
 
-    IEnumerator SpawnObject(GameObject obj, float timeInterval)
+    IEnumerator SpawnObject(ObjectPool pool, float timeInterval)
     {
         while (true)
         {
             float randomY = Random.Range(Constant.minHeight, Constant.maxHeight);
-            Instantiate(obj, new Vector3(transform.position.x, randomY, 0f), Quaternion.identity).transform.SetParent(transform);
+            pool.GetObject(transform.position.x, randomY);
             yield return new WaitForSeconds(timeInterval);
         }
     }
