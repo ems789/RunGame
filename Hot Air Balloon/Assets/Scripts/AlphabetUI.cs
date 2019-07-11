4f93f1ec3f31da3d4db0389118589e1367eb50d3
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-11 13:01:09 +0900

    알파벳 획득시 흑백 알파벳 위치에 컬러 알파벳을 표시(컬러 알파벳 구현)
    Gray 알파벳과 Color 알파벳을 같은 곳에 위치 시킴으로써 구현(Color 알파벳의 SetActive 값을 변경해서 표현)

---
+++ Hot Air Balloon/Assets/Scripts/AlphabetUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphabetUI : MonoBehaviour
+{
+    public GameObject[] alphabet;
+
+    public void CheckAlphabet(Alphabet alpha)
+    {
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            if (alphabet[i].CompareTag(alpha.tag)) // 받아온 알파벳의 태그가 UI의 태그와 일치하면
+            {
+                alphabet[i].SetActive(true); // 컬러 이미지 활성화
+            }
+        }
+    }
+}
