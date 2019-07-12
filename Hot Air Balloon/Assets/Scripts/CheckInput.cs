cd342659319d0778b2ca9f83edb17cf8ec9045c2
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-12 23:38:13 +0900

    입력을 검사하기 위한 클래스 CheckInput.cs 추가

---
+++ Hot Air Balloon/Assets/Scripts/CheckInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 텍스트에 입력한 문자열이 올바른지 체크를 하는 클래스
+public class CheckInput : MonoBehaviour
+{
+    public Text text;
+
+    public void Check()
+    {
+        // 텍스트 검사하는 작업, 경고 UI를 띄우는 작업 필요
+        // 이름을 저장하는 작업 필요
+        GetComponent<SceneMove>().NextScene();
+    }
+}
