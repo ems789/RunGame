afcf1c007da360ab7cc8c5e0659369412a8db7ad
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-12 23:38:32 +0900

    씬 이동을 위한 클래스 SceneMove 추가

---
+++ Hot Air Balloon/Assets/Scripts/SceneMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public int sceneNum;
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
