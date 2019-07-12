e7443438197a9b6c1103315454c95de0f3130e05
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-13 01:06:29 +0900

    GameManager에 일시정지 상태를 관리하는 bool 변수 추가

--- Hot Air Balloon/Assets/Scripts/GameManager.cs
+++ Hot Air Balloon/Assets/Scripts/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
     public Text coinNum;
     private int coin;
 
+    public bool isPause = false;
+
     private void Awake()
     {
         if (instance == null)
