d2d08d1f7691c6828c8cfe5b287315d8b9958231
ems789 <32154246+ems789@users.noreply.github.com>
2019-07-16 03:56:29 +0900

    ScrollingObject의 움직임을  제어하는 함수 추가

--- Hot Air Balloon/Assets/Scripts/ScrollingObject.cs
+++ Hot Air Balloon/Assets/Scripts/ScrollingObject.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class ScrollingObject : MonoBehaviour
 {
     public float speed = 5f;
+    private float tempSpeed = 0;
+
+    private void Start()
+    {
+        tempSpeed = speed;
+    }
 
     void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    public void Stop()
+    {
+        speed = 0;
+    }
+
+    public void Restart()
+    {
+        speed = tempSpeed;
     }
 }
