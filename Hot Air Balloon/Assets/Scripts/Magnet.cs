aa83384151f16d5d234a4debfef88f681ab80fd6
ems789 <32154246+ems789@users.noreply.github.com>
2019-08-03 06:43:34 +0900

    자기장에 접근한 아이템들을 끌어오는 코드 추가
    아이템이 플레이어 뒤로 넘어가면 플레이어에게 따라 붙지 못하기 떄문에 근접했을 경우 아이템이 딸려오는 속도를 늘림

---
+++ Hot Air Balloon/Assets/Scripts/Magnet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    public float magneticForce = 2f;
+
+    private void OnTriggerStay(Collider other)
+    {        
+        if (other.gameObject.layer == LayerMask.NameToLayer("Magnetic"))
+        {
+            // 일정 속도로 대상
+            other.transform.position = Vector3.Lerp(other.transform.position, transform.position, magneticForce * Time.deltaTime); 
+
+            // 대상과 거리가 근접한 경우
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            if(distance <= 2.5f)
+            {
+                // 빠른 속도로 대상에 붙음
+                other.transform.position = Vector3.Lerp(other.transform.position, transform.position, 10 * Time.deltaTime);
+            }
+
+        }
+    }
+}
