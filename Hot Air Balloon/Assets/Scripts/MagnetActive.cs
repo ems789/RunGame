a36bb7e0c8e30a1b4a8f7aba8856e85de34a761e
ems789 <32154246+ems789@users.noreply.github.com>
2019-08-03 07:15:56 +0900

    자석 제작
    자석과 충돌 시 플레이어의 자기장을 활성화시킴

---
+++ Hot Air Balloon/Assets/Scripts/MagnetActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetActive : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // 플레이어의 자기장을 활성화
+            Player.instance.GetComponentInChildren<Magnet>().StartCoroutine("MagnetFieldActive"); 
+        }
+    }
+
+    
+
+}
