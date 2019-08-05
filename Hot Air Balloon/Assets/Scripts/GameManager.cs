3ca3f654d9cfb0d338019bb4f32f8368d128971b
ems789 <32154246+ems789@users.noreply.github.com>
2019-08-05 14:22:31 +0900

    GamaManager.cs에서 데이터를 저장, 로드하는 코드를 호출
    게임을 시작 시 저장한 데이터가 존재하면 데이터를 불러와 캐릭터의 레벨, 목표거리, 최고기록을 설정.
    
    게임 종료 시 데이터를 저장.

--- Hot Air Balloon/Assets/Scripts/GameManager.cs
+++ Hot Air Balloon/Assets/Scripts/GameManager.cs
@@ -16,6 +16,8 @@ public class GameManager : MonoBehaviour
     public Text coinScore;
     public Text totalScore;
 
+    private int highestScore = 0; // 게임이 끝난 뒤 최고기록 표시
+
     const int scorePerCoin = 100;
     const int scorePerDistance = 20;
 
@@ -30,6 +32,15 @@ public class GameManager : MonoBehaviour
             instance = this;
         else
             Destroy(gameObject);
+
+        // 저장된 데이터를 로드
+        DataManager.instance.Load();
+        if (DataManager.instance.data != null) // 저장된 데이터가 존재하면
+        {
+            Player.instance.level = DataManager.instance.data.level;
+            highestScore = DataManager.instance.data.highestScore;
+            PlayerMove.instance.targetDistance = DataManager.instance.data.targetDistance;
+        }
         
         Screen.SetResolution(800, 480, false); // 스크린 해상도 고정
         Application.targetFrameRate = 60; // 게임 프레임을 60으로 설정
@@ -47,11 +58,26 @@ public class GameManager : MonoBehaviour
 
         // 플레이어가 가지고 있는 PlayerMove 스크립트에서 이동한 거리를 불러옴
         int distance = (int)GameObject.Find("Player").GetComponent<PlayerMove>().curDistance;
+
         // 점수를 계산하고 결과창을 띄움
         distanceScore.text = (distance * scorePerDistance).ToString();
         coinScore.text = (coin * scorePerCoin).ToString();
-        totalScore.text = "Total Score : " + ((distance * scorePerDistance) + (coin * scorePerCoin)).ToString();
+        totalScore.text = ((distance * scorePerDistance) + (coin * scorePerCoin)).ToString();
         gameResultBoard.SetActive(true);
+
+        // 최고 기록을 갱신한 경우
+        if (int.Parse(totalScore.text) > highestScore)
+        {
+            highestScore = int.Parse(totalScore.text);
+        }
+
+        // 데이터 저장
+        GameData data = new GameData();
+        data.level = Player.instance.level;
+        data.highestScore = highestScore;
+        data.targetDistance = PlayerMove.instance.targetDistance;        
+        DataManager.instance.Save(data);
+
         Time.timeScale = 0;
     }
 }
