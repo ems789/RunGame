612ec428aa6a8377f43e32d3505b012e3fd7538d
ems789 <32154246+ems789@users.noreply.github.com>
2019-08-09 11:04:09 +0900

    SoundManager.cs 추가
    한 번 실행, 정해진 시간동안 반복 실행하는 두가지 함수

---
+++ Hot Air Balloon/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+
+    // 효과음
+    public AudioClip collision;
+    public AudioClip getCoin;
+    public AudioClip getItem;
+    public AudioClip airplaneWarning;
+    public AudioClip airplaneDeparture;
+    public AudioClip levelUp;
+
+    // 효과음 처리용 AudioSource
+    public AudioSource EFXSource;
+    public AudioSource EFXSourceRepeat;
+
+    #region singleton
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+    #endregion
+
+    public void PlayOnce(AudioClip clip)
+    {
+        EFXSource.PlayOneShot(clip);
+    }
+
+    // 사운드를 일정 시간동안만 반복
+    public IEnumerator PlayForNSeconds(AudioClip clip, float time)
+    {       
+        EFXSourceRepeat.loop = true;
+        EFXSourceRepeat.clip = clip;
+        EFXSourceRepeat.Play();
+        yield return new WaitForSeconds(time);
+
+        EFXSourceRepeat.loop = false;
+    }
+}
