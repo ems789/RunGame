03a854aa47bcf92b7431311133ff1552f0c685ae
ems789 <32154246+ems789@users.noreply.github.com>
2019-08-11 02:10:55 +0900

    이펙트 호출 시 사용하기 위한 Enum 클래스 제작
    이펙트 배열에 접근할 때 숫자로 접근하면 혼동할 수 있으므로 불러올 이펙트를 명확하게 구분하기 위함

---
+++ Hot Air Balloon/Assets/Scripts/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enum
+{
+    public enum Particle
+    {
+        cloudCollision,
+        crowCollision,
+        gameDead,
+        booster,
+        levelUp,
+        magnet,
+        resurrection,
+    }
+}
